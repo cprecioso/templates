b78300869e4ce3b234026df62654e2f2e2fa95d9
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-04-12 14:37:48 +0200

    Fix package name resolution

--- pkgs/cli/src/pkgName.ts
+++ pkgs/cli/src/pkgName.ts
@@ -3,16 +3,19 @@ import slugify from "@sindresorhus/slugify"
 export const extractPkgName = (input: string) => {
   if (input.startsWith("@")) {
     const slashIndex = input.indexOf("/")
-    return {
-      scope: input.slice(1, slashIndex),
-      pkgName: slashIndex > 0 ? input.slice(slashIndex) : "",
-    }
+    return slashIndex > 0
+      ? {
+          scope: input.slice(1, slashIndex),
+          pkgName: input.slice(slashIndex + 1),
+        }
+      : { scope: input.slice(1) }
   } else {
     return { pkgName: input }
   }
 }
 
-const slugifyOptions: slugify.Options = { decamelize: false, lowercase: true }
+const slugifyOptions: slugify.Options = { decamelize: true, lowercase: true }
+
 export const resolvePkgName = (initial: string, input: string) => {
   let { scope, pkgName } = extractPkgName(input)
   if (scope) scope = slugify(scope, slugifyOptions)
